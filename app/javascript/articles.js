2f7e2cc3d4624b33ea473a380d7084b8556cacd5
ninjyasoppy <69029725+ninjyasoppy@users.noreply.github.com>
2020-08-19 15:54:30 +0900

    マウスオーバーすると色が変わる機能

---
+++ app/javascript/articles.js
@@ -0,0 +1,9 @@
+$(function () {
+  $(document)
+    .on("mouseover", "h1", function () {
+      $(this).css({ color: "blue" });
+    })
+    .on("mouseout", "h1", function () {
+      $(this).css({ color: "" });
+    });
+});
